940869f430ea5748393a097d180f019351e16018
Leekukju <ckzmfk19@kakao.com>
2023-09-18 10:06:57 +0900

    라우터 실습 끝

--- src/router-prac/RouterApp.jsx
+++ src/router-prac/RouterApp.jsx
@@ -5,6 +5,9 @@ import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
 import Layout from "./pages/Layout";
+import NotFound from "./pages/NotFound";
+import MyPage from "./pages/MyPage";
+import Login from "./pages/Login";
 
 const RouterApp = () => {
   return (
@@ -17,6 +20,9 @@ const RouterApp = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/mypage" element={<MyPage/>}/>
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
   )
 }
